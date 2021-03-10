816ead275d3044c333639a7136744190528f0068
Vladimir Anatolevich Gorbachev <66027907+sinfwave@users.noreply.github.com>
2021-03-10 14:07:17 +0300

    Update showXmlData.php

--- showXmlData.php
+++ showXmlData.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace Kommersant;
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
+
 /**
  * Constructed class. Takes test xml file as construct argument for using in class methods
  * 
